2b9945d9bab9bed5f4e2c6e8d72965e793f3c662
Arvind Satyanarayan <arvindsatya1@gmail.com>
2017-01-04 10:53:10 +0530

    Update travel.

--- src/data/travel.js
+++ src/data/travel.js
@@ -1,4 +1,32 @@
 module.exports = [
+ {
+    start: '2017-04-24',
+    end: '2017-04-25',
+    desc: 'OpenVis Conf',
+    url: 'http://openvisconf.com',
+    location: 'Boston, MA'
+  },
+  {
+    start: '2017-02-07',
+    end: '2017-02-08',
+    desc: 'Cornell University',
+    url: 'http://cs.cornell.edu',
+    location: 'Ithaca, NY'
+  },
+  {
+    start: '2017-02-02',
+    end: '2017-02-03',
+    desc: 'UW Madison',
+    url: 'http://cs.wisc.edu',
+    location: 'Madison, WI'
+  },
+  {
+    start: '2017-01-17',
+    end: '2017-01-21',
+    desc: 'Moore Early Career Symposium',
+    url: 'https://github.com/DDD-Moore/early-career-hawaii/blob/master/README.md',
+    location: 'Hawaii'
+  },
   {
     start: '2016-10-27',
     end: '2016-10-28',
@@ -12,41 +40,41 @@ module.exports = [
     desc: 'IEEE VIS',
     url: 'http://ieeevis.org',
     location: 'Baltimore, MD'
-  },
-  {
-    start: '2016-07-18',
-    end: '2016-07-21',
-    desc: 'Intel (Big Data ISTC)',
-    url: 'http://istc-bigdata.org',
-    location: 'Cambridge, MA'
-  },
-  {
-    start: '2016-07-08',
-    end: '2016-07-10',
-    desc: 'DataViz Camp',
-    url: 'http://dataviz.camp',
-    location: 'United Nations'
-  },
-  {
-    start: '2016-06-06',
-    end: '2016-06-10',
-    desc: 'EuroVis',
-    url: 'http://www.cs.rug.nl/jbi/eurovis2016/',
-    location: 'Groningen, Netherlands'
-  },
-  {
-    start: '2016-05-16',
-    end: '2016-06-04',
-    desc: 'ExSitu, INRIA',
-    url: 'http://www.inria.fr/en/teams/ex-situ',
-    location: 'Saclay, France'
-  },
-  {
-    start: '2016-04-24',
-    end: '2016-04-26',
-    desc: 'OpenVis Conf',
-    url: 'http://openvisconf.com',
-    location: 'Boston, MA'
+  // },
+  // {
+  //   start: '2016-07-18',
+  //   end: '2016-07-21',
+  //   desc: 'Intel (Big Data ISTC)',
+  //   url: 'http://istc-bigdata.org',
+  //   location: 'Cambridge, MA'
+  // },
+  // {
+  //   start: '2016-07-08',
+  //   end: '2016-07-10',
+  //   desc: 'DataViz Camp',
+  //   url: 'http://dataviz.camp',
+  //   location: 'United Nations'
+  // },
+  // {
+  //   start: '2016-06-06',
+  //   end: '2016-06-10',
+  //   desc: 'EuroVis',
+  //   url: 'http://www.cs.rug.nl/jbi/eurovis2016/',
+  //   location: 'Groningen, Netherlands'
+  // },
+  // {
+  //   start: '2016-05-16',
+  //   end: '2016-06-04',
+  //   desc: 'ExSitu, INRIA',
+  //   url: 'http://www.inria.fr/en/teams/ex-situ',
+  //   location: 'Saclay, France'
+  // },
+  // {
+  //   start: '2016-04-24',
+  //   end: '2016-04-26',
+  //   desc: 'OpenVis Conf',
+  //   url: 'http://openvisconf.com',
+  //   location: 'Boston, MA'
   // },
   // {
   //   start: '2015-10-25',
